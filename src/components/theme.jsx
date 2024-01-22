3401bbdf0d8a7330654ec585f96060417c4eac02
j-Hop <119629763+j-Hop@users.noreply.github.com>
2024-01-22 16:03:04 +0200

    add folders and file

---
+++ src/components/theme.jsx
@@ -0,0 +1,14 @@
+export const theme = {
+    color:{
+       white:"#FFFFFF",
+       dark:"#2F2F2F",
+       blue:"#3470FF",
+       navyblue:"#0B44CD",
+       bginput:"#F7F7FB",
+       tagtext:"rgba(18, 20, 23, .5);",
+       buttontext:"#8A8A89",
+       modaltext:"#363535",
+    },
+    spacing: (value) => `${value*2}px`,
+    transition: "250s linear",
+};
